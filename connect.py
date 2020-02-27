a542c2cefba1f2d124bab1b44367824682acfedf
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2020-02-27 18:26:49 -0500

    a

--- connect.py
+++ connect.py
@@ -1,7 +1,6 @@
 from socket import socket, AF_INET, SOCK_STREAM, gethostname, gethostbyname
 import os.path
 import time
-import textwrap
 
 HOST = ''
 PORT = 8000
@@ -10,26 +9,26 @@ IPv4 = gethostbyname(gethostname())
 print(f'Website client currently running at {IPv4} on port {PORT}')
 
 with open(os.path.join(os.path.dirname(__file__), 'index.html'), 'rb') as file:
-    html = file.read()
+    html = file.read()  # read the html file
 
 with open(os.path.join(os.path.dirname(__file__), 'favicon.png'), 'rb') as file:
-    favicon = file.read()
+    favicon = file.read()  # read the favicon
 
-with socket(AF_INET, SOCK_STREAM) as sock:
+with socket(AF_INET, SOCK_STREAM) as sock:  # create a socket named sock
     sock.bind((HOST, PORT))
-    sock.listen()
+    sock.listen()  # listen for incoming connection requests
     while 1:
-        conn, addr = sock.accept()
+        conn, addr = sock.accept()  # blocks code until there is a connection
         with conn:
-            data = conn.recv(1024)
-            split_data = data.decode().split(' ')
+            data = conn.recv(1024)  # get the request
+            split_data = data.decode().split(' ')  # make the http request parts into a list
             try:
-                requested_page = split_data[1]
-                print('\nPAGE PAGE:' + requested_page)
-            except IndexError:
+                requested_page = split_data[1]  # the second part of the http browser request ill be /somepage or just / if no slash is in the url
+                print('\nPAGE:' + requested_page)
+            except IndexError:  # I don't think this is still necessary
                 print('\n' + data.decode())
             # print(requested_page)
-            if requested_page == '/':
+            if requested_page == '/':  # no slash in the url
                 conn.sendall(b'''HTTP/1.1 200 OK
 Content-Type: text/html
 \n''' + html)
