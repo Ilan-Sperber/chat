8c0f41536965b602cd3087fe735742cd6a0463ba
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2020-02-27 18:22:07 -0500

    a

--- connect.py
+++ connect.py
@@ -1,11 +1,19 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, gethostname, gethostbyname
 import os.path
+import time
+import textwrap
+
+HOST = ''
+PORT = 8000
+
+IPv4 = gethostbyname(gethostname())
+print(f'Website client currently running at {IPv4} on port {PORT}')
 
 with open(os.path.join(os.path.dirname(__file__), 'index.html'), 'rb') as file:
     html = file.read()
 
-HOST = ''
-PORT = 80
+with open(os.path.join(os.path.dirname(__file__), 'favicon.png'), 'rb') as file:
+    favicon = file.read()
 
 with socket(AF_INET, SOCK_STREAM) as sock:
     sock.bind((HOST, PORT))
@@ -14,4 +22,19 @@ with socket(AF_INET, SOCK_STREAM) as sock:
         conn, addr = sock.accept()
         with conn:
             data = conn.recv(1024)
-            conn.sendall(b'HTTP/1.1 200 OK' + html)
+            split_data = data.decode().split(' ')
+            try:
+                requested_page = split_data[1]
+                print('\nPAGE PAGE:' + requested_page)
+            except IndexError:
+                print('\n' + data.decode())
+            # print(requested_page)
+            if requested_page == '/':
+                conn.sendall(b'''HTTP/1.1 200 OK
+Content-Type: text/html
+\n''' + html)
+
+            elif requested_page == '/favicon.ico':
+                conn.sendall(b'''HTTP/1.1 200 OK
+Content-Type: image/apng
+\n''' + favicon)
